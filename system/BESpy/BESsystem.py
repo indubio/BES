990251421ca151ffb747ab34c6396fabccdcc7c0
Steffen Eichhorn <mail@indubio.org>
2013-05-14 15:02:08 +0200

    Klasse ReportProperties hinzugefügt

--- system/BESpy/BESsystem.py
+++ system/BESpy/BESsystem.py
@@ -7,6 +7,8 @@ BES system connector
 
 import os
 import ConfigParser
+import email
+import MySQLdb
 
 class BESConfig:
     def __init__(self, defaults=None):
@@ -20,6 +22,70 @@ class BESConfig:
 
     def get(self, section, option):
         return self.Config.get(section, option)
+
+class ReportProperties (object):
+    def __init__(self, reportname):
+        self.__reportname = reportname
+        conf = BESConfig()
+
+        # connect to BES DB
+        mysql  = MySQLdb.connect(
+            conf.get('mysql', 'dbhost'),
+            conf.get('mysql', 'user'),
+            conf.get('mysql', 'pass'),
+            conf.get('mysql', 'dbname'),
+            cursorclass=MySQLdb.cursors.DictCursor
+            )
+        cursor = mysql.cursor()
+        sql = "set character set utf8"
+        cursor.execute(sql)
+        sqlquery = """
+            select * from reports
+            where name = '%s'
+        """ % (self.__reportname)
+        cursor.execute(sqlquery)
+        self.__propertiesdata = cursor.fetchone()
+        self.__cc_recipient = []
+        sqlquery = """
+            select report_recipients.*,
+                user.familienname as lastname,
+                user.vorname as firstname,
+                user.email as email
+            from report_recipients
+            left outer join user on (user.ID = report_recipients.userID)
+            where reportID = '%s'
+        """ % (self.__propertiesdata['ID'])
+        cursor.execute(sqlquery)
+        for recipient in cursor.fetchall():
+            self.__cc_recipient.append(email.utils.formataddr((
+                ", ".join((
+                    recipient['lastname'],
+                    recipient['firstname']
+                    )),
+                recipient['email']
+                )))
+        cursor.close()
+        mysql.close()
+
+    def recipient(self):
+        return self.__propertiesdata['mail_to']
+
+    def recipients_CC(self):
+        return self.__cc_recipient
+
+    def sender(self):
+        return self.__propertiesdata['mail_from']
+
+    def mailsubject(self):
+        return self.__propertiesdata['mail_head']
+
+    def mailbody(self, bodytype='plain'):
+        if bodytype == 'plain':
+            return self.__propertiesdata['mail_body_plain']
+        elif bodytype == 'html':
+            return self.__propertiesdata['mail_body_html']
+        else:
+            self.__propertiesdata['mail_body_plain']
 
 def getBES_SQLConnection():
     pass
