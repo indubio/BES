4fc7bb45c23e6fe9797781f481a80e4bec10fda3
Steffen Eichhorn <mail@indubio.org>
2014-01-06 13:24:07 +0100

    [CodeStyling] - Tabulatoren durch Leerzeichen ersetzt - Extrem lange Zeilen gekürzt

--- html/ajax_get_badolist.php
+++ html/ajax_get_badolist.php
@@ -5,14 +5,15 @@ include ('bes_init.php');
  * Authentication
  */
 if ($_SESSION['logedin'] != 1 or auth($_SESSION['userlevel'], PAGE_PATLIST) !=1 ) {
-	echo '{'
-		.'"sError":"auth_error",'
-		.'"sEcho":1,'
-		.'"iTotalRecords":0,'
-		.'"iTotalDisplayRecords":0,'
-		.'"sColumns":"ReferralID,ReferralType,ReferralDate,Facility,Referrer,NHI,Patient,ReviewStatus",'
-		.'"aaData":[]}';
-	exit;
+    echo '{'
+        .'"sError":"auth_error",'
+        .'"sEcho":1,'
+        .'"iTotalRecords":0,'
+        .'"iTotalDisplayRecords":0,'
+        .'"sColumns":"ReferralID,ReferralType,ReferralDate,Facility,'
+                .'Referrer,NHI,Patient,ReviewStatus",'
+        .'"aaData":[]}';
+    exit;
 }
 
 /*
@@ -22,8 +23,8 @@ $_POST = escape_and_clear($_POST);
 $_GET = escape_and_clear($_GET);
 
 $aColumns = array(
-	'ID', 'db_tbl', 'familienname', 'vorname', 'geburtsdatum',
-	'aufnahmenummer','aufnahmedatum','behandler_char', 'station_char'
+    'ID', 'db_tbl', 'familienname', 'vorname', 'geburtsdatum',
+    'aufnahmenummer','aufnahmedatum','behandler_char', 'station_char'
 );
 
 /*
@@ -31,92 +32,123 @@ $aColumns = array(
  */
 $sLimit = "";
 if ( isset( $_GET['iDisplayStart'] ) && $_GET['iDisplayLength'] != '-1' ) {
-	$sLimit = "LIMIT ".mysql_real_escape_string( $_GET['iDisplayStart'] ).", ".
-		mysql_real_escape_string( $_GET['iDisplayLength'] );
+    $sLimit = "LIMIT ".mysql_real_escape_string( $_GET['iDisplayStart'] ).", ".
+        mysql_real_escape_string( $_GET['iDisplayLength'] );
 }
 
 /*
  * Ordering
  */
 if ( isset( $_GET['iSortCol_0'] ) ) {
-	$sOrder = "ORDER BY  ";
-	for ( $i = 0 ; $i < intval( $_GET['iSortingCols'] ); $i++ ) {
-		switch (fnColumnToField(mysql_real_escape_string( $_GET['iSortCol_'.$i] ))) {
-			case "u_geburtsdatum":
-				$sOrder .= "str_to_date(u_geburtsdatum,'%d.%m.%Y') ";
-				break;
-			case "u_aufnahmedatum":
-				$sOrder .= "str_to_date(u_aufnahmedatum,'%d.%m.%Y') ";
-				break;
-			case "u_entlassungsdatum":
-				$sOrder .= "str_to_date(u_entlassungsdatum,'%d.%m.%Y') ";
-				break;
-			default:
-				$sOrder .= fnColumnToField(mysql_real_escape_string( $_GET['iSortCol_'.$i] ));
-				break;
-		}
-		$sOrder .= " ".mysql_real_escape_string( $_GET['sSortDir_'.$i] ) .", ";
-	}
-	$sOrder = substr_replace( $sOrder, "", -2 );
+    $sOrder = "ORDER BY  ";
+    for ( $i = 0 ; $i < intval( $_GET['iSortingCols'] ); $i++ ) {
+        switch (fnColumnToField(mysql_real_escape_string( $_GET['iSortCol_'.$i] ))) {
+            case "u_geburtsdatum":
+                $sOrder .= "str_to_date(u_geburtsdatum,'%d.%m.%Y') ";
+                break;
+            case "u_aufnahmedatum":
+                $sOrder .= "str_to_date(u_aufnahmedatum,'%d.%m.%Y') ";
+                break;
+            case "u_entlassungsdatum":
+                $sOrder .= "str_to_date(u_entlassungsdatum,'%d.%m.%Y') ";
+                break;
+            default:
+                $sOrder .= fnColumnToField(mysql_real_escape_string( $_GET['iSortCol_'.$i] ));
+                break;
+        }
+        $sOrder .= " ".mysql_real_escape_string( $_GET['sSortDir_'.$i] ) .", ";
+    }
+    $sOrder = substr_replace( $sOrder, "", -2 );
 }
 
 /*
  * Filtering
  */ 
-$sWheretotal1 = "WHERE (geschlossen=0 OR (geschlossen=1 AND entlassungsdatum='')) AND (datamigration!=1) AND (`cancelled`=0)";
+$sWheretotal1 = "WHERE (geschlossen=0 OR (geschlossen=1 AND entlassungsdatum='')) "
+    ."AND (datamigration!=1) AND (`cancelled`=0)";
 $sWheretotal2 = "WHERE `geschlossen`='0' AND (`badotyp`='1' OR `badotyp`='2')";
 
 /* Station per Get oder nach Userzuweisung */
 if (isset($_GET['selstation']) and ($_GET['selstation'] != "undefined")){
-	$selectedstation=$_GET['selstation'];
-	if ($selectedstation < 0) {
-		$selectedstation = $_SESSION['stationsid'];
-	}
+    $selectedstation=$_GET['selstation'];
+    if ($selectedstation < 0) {
+        $selectedstation = $_SESSION['stationsid'];
+    }
 } else {
-	$selectedstation = $_SESSION['stationsid'];
-	if ($_SESSION['userlevel'] == UG_STATUSER or $_SESSION['userlevel'] == UG_ADMIN or $selectedstation == -1) {
-		$selectedstation=99;
-	}
+    $selectedstation = $_SESSION['stationsid'];
+    if ($_SESSION['userlevel'] == UG_STATUSER or
+        $_SESSION['userlevel'] == UG_ADMIN or
+        $selectedstation == -1) { $selectedstation=99; }
 }
 
 if ($selectedstation == 0 or $selectedstation == 99) {
-	if ($selectedstation == 0) {
-		/* behandler select */
-		$sWheretotal1 .=" AND (behandler=".$_SESSION["userid"].")";
-		$sWheretotal2 .=" AND (behandler=".$_SESSION["userid"].")";
-	}
+    if ($selectedstation == 0) {
+        /* behandler select */
+        $sWheretotal1 .=" AND (behandler=".$_SESSION["userid"].")";
+        $sWheretotal2 .=" AND (behandler=".$_SESSION["userid"].")";
+    }
 } else {
-	/* station select */
-	if ($selectedstation >= 10) {
-		$sWheretotal1 .=" AND (1=2)"; // schließt alle aus der STAT FallDB aus
-		$sWheretotal2 .=" AND (pia_id=".($selectedstation-10).")";
-	} else {
-		$sWheretotal1 .=" AND (station_c=".$selectedstation.")";
-		$sWheretotal2 .=" AND (1=2)"; // schließt alle aus der PIA FallDB aus
-	}
+    /* station select */
+    if ($selectedstation >= 10) {
+        $sWheretotal1 .=" AND (1=2)"; // schließt alle aus der STAT FallDB aus
+        $sWheretotal2 .=" AND (pia_id=".($selectedstation-10).")";
+    } else {
+        $sWheretotal1 .=" AND (station_c=".$selectedstation.")";
+        $sWheretotal2 .=" AND (1=2)"; // schließt alle aus der PIA FallDB aus
+    }
 }
 /* "echtes" Filtering */
 $sWhere1 = $sWheretotal1;
 $sWhere2 = $sWheretotal2;
 if ( $_GET['sSearch'] != "" ) {
-	$sWhere1 .= " AND ( familienname LIKE '%".mysql_real_escape_string( $_GET['sSearch'] )."%' OR "
-		."vorname LIKE '%".mysql_real_escape_string( $_GET['sSearch'] )."%' OR "
-		."geburtsdatum LIKE '%".mysql_real_escape_string( $_GET['sSearch'] )."%' OR "
-		."aufnahmenummer LIKE '%".mysql_real_escape_string( $_GET['sSearch'] )."%' OR "
-		."aufnahmedatum LIKE '%".mysql_real_escape_string( $_GET['sSearch'] )."%')";
-	$sWhere2 .= " AND ( familienname LIKE '%".mysql_real_escape_string( $_GET['sSearch'] )."%' OR "
-		."vorname LIKE '%".mysql_real_escape_string( $_GET['sSearch'] )."%' OR "
-		."geburtsdatum LIKE '%".mysql_real_escape_string( $_GET['sSearch'] )."%' OR "
-		."soarian_aufnahmenummer LIKE '%".mysql_real_escape_string( $_GET['sSearch'] )."%' OR "
-		."aufnahmedatum LIKE '%".mysql_real_escape_string( $_GET['sSearch'] )."%')";
+    $sWhere1 .= " AND ( familienname LIKE '%".mysql_real_escape_string( $_GET['sSearch'] )."%' OR "
+        ."vorname LIKE '%".mysql_real_escape_string( $_GET['sSearch'] )."%' OR "
+        ."geburtsdatum LIKE '%".mysql_real_escape_string( $_GET['sSearch'] )."%' OR "
+        ."aufnahmenummer LIKE '%".mysql_real_escape_string( $_GET['sSearch'] )."%' OR "
+        ."aufnahmedatum LIKE '%".mysql_real_escape_string( $_GET['sSearch'] )."%')";
+    $sWhere2 .= " AND ( familienname LIKE '%".mysql_real_escape_string( $_GET['sSearch'] )."%' OR "
+        ."vorname LIKE '%".mysql_real_escape_string( $_GET['sSearch'] )."%' OR "
+        ."geburtsdatum LIKE '%".mysql_real_escape_string( $_GET['sSearch'] )."%' OR "
+        ."soarian_aufnahmenummer LIKE '%".mysql_real_escape_string( $_GET['sSearch'] )."%' OR "
+        ."aufnahmedatum LIKE '%".mysql_real_escape_string( $_GET['sSearch'] )."%')";
 }
 
 /*
  * SQL queries
  * Get data to display
  */
-$sQuery  = "(SELECT '1' as `u_db_tbl`, `ID` as `u_ID`, `familienname` as `u_familienname`, `vorname` as `u_vorname`, `behandler` as `u_behandler`, `station_c` as `u_station`, `geburtsdatum` as `u_geburtsdatum`, `geschlossen` as `u_geschlossen`, `datamigration` as `u_datamigration`, `reopen` as `u_reopen`, '0' as `u_badotyp` ,`aufnahmedatum` as `u_aufnahmedatum`, `entlassungsdatum` as `u_entlassungsdatum`, `aufnahmenummer` as `u_aufnahmenummer`, `last_change` as `u_last_change`, `entlassungsdatum` as `u_entlassdatum` from fall $sWhere1) UNION ";
-$sQuery .= "(SELECT '2' as `u_db_tbl`, `ID` as `u_ID`, `familienname` as `u_familienname`, `vorname` as `u_vorname`, `behandler` as `u_behandler`, `pia_id`    as `u_station`, `geburtsdatum` as `u_geburtsdatum`, `geschlossen` as `u_geschlossen`, '0' as `u_datamigration`, '0' as `u_reopen`, `badotyp` as `u_badotyp`,`aufnahmedatum` as `u_aufnahmedatum`, `entlassdatum` as `u_entlassungsdatum`, `soarian_aufnahmenummer` as `u_aufnahmenummer`, `last_change` as `u_last_change`, `entlassdatum` as `u_entlassdatum` from fall_pia $sWhere2) $sOrder $sLimit";
+$sQuery  = "(SELECT '1' as `u_db_tbl`, `ID` as `u_ID`, "
+    ."`familienname` as `u_familienname`,"
+    ."`vorname` as `u_vorname`,"
+    ."`behandler` as `u_behandler`,"
+    ."`station_c` as `u_station`,"
+    ."`geburtsdatum` as `u_geburtsdatum`,"
+    ."`geschlossen` as `u_geschlossen`,"
+    ."`datamigration` as `u_datamigration`,"
+    ."`reopen` as `u_reopen`,"
+    ."'0' as `u_badotyp`,"
+    ."`aufnahmedatum` as `u_aufnahmedatum`,"
+    ."`entlassungsdatum` as `u_entlassungsdatum`,"
+    ."`aufnahmenummer` as `u_aufnahmenummer`,"
+    ."`last_change` as `u_last_change`,"
+    ."`entlassungsdatum` as `u_entlassdatum`"
+    ." from fall $sWhere1) UNION "
+    ."(SELECT '2' as `u_db_tbl`, `ID` as `u_ID`,"
+    ."`familienname` as `u_familienname`,"
+    ."`vorname` as `u_vorname`,"
+    ."`behandler` as `u_behandler`,"
+    ."`pia_id` as `u_station`,"
+    ."`geburtsdatum` as `u_geburtsdatum`,"
+    ."`geschlossen` as `u_geschlossen`,"
+    ."'0' as `u_datamigration`,"
+    ."'0' as `u_reopen`,"
+    ."`badotyp` as `u_badotyp`,"
+    ."`aufnahmedatum` as `u_aufnahmedatum`,"
+    ."`entlassdatum` as `u_entlassungsdatum`,"
+    ."`soarian_aufnahmenummer` as `u_aufnahmenummer`,"
+    ."`last_change` as `u_last_change`,"
+    ."`entlassdatum` as `u_entlassdatum`"
+    ." from fall_pia $sWhere2) $sOrder $sLimit";
 
 $rResult = mysql_query( $sQuery) or die(mysql_error());
 
@@ -127,7 +159,9 @@ $aResultFilterTotal = mysql_fetch_array($rResultFilterTotal);
 $iFilteredTotal = $aResultFilterTotal[0];
 
 /* Total data set length */
-$sQuery = "SELECT sum(num_ids) FROM ((SELECT COUNT('ID') as num_ids FROM fall $sWheretotal1) UNION (SELECT COUNT('ID') as num_ids FROM fall_pia $sWheretotal2)) AS countsubset";
+$sQuery = "SELECT sum(num_ids) FROM ((SELECT COUNT('ID') as num_ids FROM "
+    ."fall $sWheretotal1) UNION (SELECT COUNT('ID') as num_ids FROM "
+    ."fall_pia $sWheretotal2)) AS countsubset";
 $rResultTotal = mysql_query( $sQuery) or die(mysql_error());
 $aResultTotal = mysql_fetch_array($rResultTotal);
 $iTotal = $aResultTotal[0];
@@ -141,60 +175,63 @@ $sOutput .= '"iTotalRecords": '.$iTotal.', ';
 $sOutput .= '"iTotalDisplayRecords": '.$iFilteredTotal.', ';
 $sOutput .= '"aaData": [ ';
 while ( $aRow = mysql_fetch_array( $rResult ) ){
-	$sOutput .= "[";
-	/* Statusspalte */
-	$statusdummy="&nbsp;";
-	if ($aRow['u_reopen'] == 1) {
-		$statusdummy = "R";
-	} else {
-		if ($aRow['u_last_change'] == "0000-00-00 00:00:00") { $statusdummy = "A"; }
-		if ($aRow['u_entlassdatum'] != ""){ $statusdummy = "E"; }
-	}
-	if ($aRow['u_db_tbl'] == 2){
-		if ($aRow['u_badotyp'] == 1) { $statusdummy = "S"; }
-		if ($aRow['u_badotyp'] == 2) { $statusdummy = "V"; }
-	}
-	$sOutput .= '"'.$statusdummy.'",';
-	/* Spaltendaten */
-	$aRow['u_familienname'] = htmlspecialchars($aRow['u_familienname'], ENT_NOQUOTES, 'UTF-8');
-	$aRow['u_vorname'] = htmlspecialchars($aRow['u_vorname'], ENT_NOQUOTES, 'UTF-8');
-	if ($aRow['u_behandler'] == '-1') {
-		$aRow['u_behandler_char'] = "&nbsp;";
-	} else {
-		$aRow['u_behandler_char'] = idtostr($aRow['u_behandler'], "user", "username");
-	}
-	if ($aRow['u_station'] == '-1') {
-		$aRow['u_station_char'] = "&nbsp;";
-	} else {
-		if ($aRow['u_db_tbl'] == 1) {
-			$aRow['u_station_char'] = idtostr($aRow['u_station'] ,"f_psy_stationen");
-		}
-		if ($aRow['u_db_tbl'] == 2) {
-			$aRow['u_station_char'] = idtostr($aRow['u_station'], "f_psy_ambulanzen");
-		}
-	}
-	if ($aRow['u_geburtsdatum'] == '') { $u_aRow['geburtsdatum'] = "unbekannt"; }
-	for ( $i = 0 ; $i < count($aColumns) ; $i++ ){
-		$sOutput .= '"'.str_replace('"', '\"', $aRow[ "u_".$aColumns[$i] ]).'",';
-	}
-	$sOutput = substr_replace( $sOutput, "", -1 );
-	$sOutput .= "],";
+    $sOutput .= "[";
+    /* Statusspalte */
+    $statusdummy="&nbsp;";
+    if ($aRow['u_reopen'] == 1) {
+        $statusdummy = "R";
+    } else {
+        if ($aRow['u_last_change'] == "0000-00-00 00:00:00") {
+            $statusdummy = "A";
+        }
+        if ($aRow['u_entlassdatum'] != "") {
+            $statusdummy = "E";
+        }
+    }
+    if ($aRow['u_db_tbl'] == 2){
+        if ($aRow['u_badotyp'] == 1) { $statusdummy = "S"; }
+        if ($aRow['u_badotyp'] == 2) { $statusdummy = "V"; }
+    }
+    $sOutput .= '"'.$statusdummy.'",';
+    /* Spaltendaten */
+    $aRow['u_familienname'] = htmlspecialchars($aRow['u_familienname'], ENT_NOQUOTES, 'UTF-8');
+    $aRow['u_vorname'] = htmlspecialchars($aRow['u_vorname'], ENT_NOQUOTES, 'UTF-8');
+    if ($aRow['u_behandler'] == '-1') {
+        $aRow['u_behandler_char'] = "&nbsp;";
+    } else {
+        $aRow['u_behandler_char'] = idtostr($aRow['u_behandler'], "user", "username");
+    }
+    if ($aRow['u_station'] == '-1') {
+        $aRow['u_station_char'] = "&nbsp;";
+    } else {
+        if ($aRow['u_db_tbl'] == 1) {
+            $aRow['u_station_char'] = idtostr($aRow['u_station'] ,"f_psy_stationen");
+        }
+        if ($aRow['u_db_tbl'] == 2) {
+            $aRow['u_station_char'] = idtostr($aRow['u_station'], "f_psy_ambulanzen");
+        }
+    }
+    if ($aRow['u_geburtsdatum'] == '') { $u_aRow['geburtsdatum'] = "unbekannt"; }
+    for ( $i = 0 ; $i < count($aColumns) ; $i++ ){
+        $sOutput .= '"'.str_replace('"', '\"', $aRow[ "u_".$aColumns[$i] ]).'",';
+    }
+    $sOutput = substr_replace( $sOutput, "", -1 );
+    $sOutput .= "],";
 }
 $sOutput = substr_replace( $sOutput, "", -1 );
 $sOutput .= '] }';
 echo $sOutput;
 
 function fnColumnToField( $i ) {
-	if ( $i == 3 )
-		return "u_familienname";
-	else if ( $i == 4 )
-		return "u_vorname";
-	else if ( $i == 5 )
-		return "u_geburtsdatum";
-	else if ( $i == 6 )
-		return "u_aufnahmenummer";
-	else if ( $i == 7 )
-		return "u_aufnahmedatum";
+    if ( $i == 3 )
+        return "u_familienname";
+    else if ( $i == 4 )
+        return "u_vorname";
+    else if ( $i == 5 )
+        return "u_geburtsdatum";
+    else if ( $i == 6 )
+        return "u_aufnahmenummer";
+    else if ( $i == 7 )
+        return "u_aufnahmedatum";
 }
-
 ?>
--- html/bes_badolist.php
+++ html/bes_badolist.php
@@ -1,14 +1,17 @@
 <?php
 include ('bes_init.php');
 if ($_SESSION['logedin'] != 1){
-	$smarty -> display('login.tpl');
-	exit;
+    $smarty -> display('login.tpl');
+    exit;
 }
 
 create_menu($_SESSION['userlevel']);
 
 if (auth($_SESSION['userlevel'], PAGE_PATLIST) != 1) {
-	message_die(GENERAL_ERROR, "Sie haben nicht die nötigen Rechte um diese Seite aufzurufen!","Authentifizierung");
+    message_die(GENERAL_ERROR,
+        "Sie haben nicht die nötigen Rechte um diese Seite aufzurufen!",
+        "Authentifizierung"
+    );
 }
 
 /*
@@ -20,32 +23,32 @@ $_GET = escape_and_clear($_GET);
 /*
  * WAF Variablen Check
  */
-if (mywaf($_GET)) {message_die(GENERAL_ERROR,"Eine mögliche Manipulation der Übergabeparameter wurde ".
-                                             "festgestellt und der Seitenaufruf unterbunden!<br/>".
-                                             "Wenden Sie sich bitte an einen Systembetreuer.","myWAF");}
-if (mywaf($_POST)){message_die(GENERAL_ERROR,"Eine mögliche Manipulation der Übergabeparameter wurde ".
-                                             "festgestellt und der Seitenaufruf unterbunden!<br/>".
-                                             "Wenden Sie sich bitte an einen Systembetreuer.","myWAF");}
+$waf_error_msg = "Eine mögliche Manipulation der Übergabeparameter wurde "
+    ."festgestellt und der Seitenaufruf unterbunden!<br/>"
+    ."Wenden Sie sich bitte an einen Systembetreuer.";
+
+if (mywaf($_GET)) {message_die(GENERAL_ERROR, $waf_error_msg, "myWAF");}
+if (mywaf($_POST)){message_die(GENERAL_ERROR, $waf_error_msg, "myWAF");}
 
 // Station per Get oder nach Userzuweisung
 if (isset($_GET['selstation'])) {
-	$selectedstation = $_GET['selstation'];
-	if ($selectedstation < 0) {
-		$selectedstation = $_SESSION['stationsid'];
-	}
+    $selectedstation = $_GET['selstation'];
+    if ($selectedstation < 0) {
+        $selectedstation = $_SESSION['stationsid'];
+    }
 } else {
-	$selectedstation = $_SESSION['stationsid'];
-	if ($_SESSION['userlevel'] == UG_STATUSER or $_SESSION['userlevel'] == UG_ADMIN or $selectedstation == -1) {
-		$selectedstation = 99;
-	}
+    $selectedstation = $_SESSION['stationsid'];
+    if ($_SESSION['userlevel'] == UG_STATUSER or
+        $_SESSION['userlevel'] == UG_ADMIN or
+        $selectedstation == -1) { $selectedstation = 99; }
 }
 
 // Auswahlbox erstellen
 $dummyarray_i = array();
 $dummyarray_k = array();
 if ($_SESSION["userlevel"] == 3) {
-	$dummyarray_i[] = "0";
-	$dummyarray_k[] = idtostr($_SESSION["userid"], "user", "username");
+    $dummyarray_i[] = "0";
+    $dummyarray_k[] = idtostr($_SESSION["userid"], "user", "username");
 }
 $dummyarray_i[] = "99";
 $dummyarray_k[] = "alle";
@@ -54,18 +57,18 @@ $query = "SELECT * FROM f_psy_stationen ORDER BY `option` ASC";
 $result = mysql_query($query);
 $num_psy = mysql_num_rows($result);
 for ($i = 0; $i < $num_psy; $i++) {
-	$row = mysql_fetch_array($result);
-	$dummyarray_i[] = $row['ID'];
-	$dummyarray_k[] = $row['option'];
+    $row = mysql_fetch_array($result);
+    $dummyarray_i[] = $row['ID'];
+    $dummyarray_k[] = $row['option'];
 }
 mysql_free_result($result);
 $query = "SELECT * FROM f_psy_ambulanzen ORDER BY `option` ASC";
 $result = mysql_query($query);
 $num_psy = mysql_num_rows($result);
 for ($i = 0; $i < $num_psy; $i++) {
-	$row = mysql_fetch_array($result);
-	$dummyarray_i[] = $row['ID']+10;
-	$dummyarray_k[] = $row['option'];
+    $row = mysql_fetch_array($result);
+    $dummyarray_i[] = $row['ID']+10;
+    $dummyarray_k[] = $row['option'];
 }
 mysql_free_result($result);
 
